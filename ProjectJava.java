7ec71d93af8197c309a5e545a379343ddb46f09d
Ilovejava777 <daniel_sismer@estudante.sesisenai.org.br>
2025-04-14 18:49:45 -0300

    Primeira Versão do Código

---
+++ ProjectJava.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ProjectJava {
+    
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("Hello World");
+        
+    }
+}

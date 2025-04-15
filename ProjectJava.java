a2ce6061f4e660b95a5a75f38f78cc6faea9e13a
Jose7764 <jose_a_torres@estudante.sesisenai.org.br>
2025-04-15 19:33:21 -0300

    feat: tela inicial 'Benvindo'

--- ProjectJava.java
+++ ProjectJava.java
@@ -8,6 +8,28 @@ public class ProjectJava {
 
         System.out.println("Hello World");
         
-        input.close();
+        System.out.println(".______    _______ .___  ___. ____    ____  __  .__   __.  _______   ______  ");
+        System.out.println("|   _  \\  |   ____||   \\/   | \\   \\  /   / |  | |  \\ |  | |       \\ /  __  \\ ");
+        System.out.println("|  |_)  | |  |__   |  \\  /  |  \\   \\/   /  |  | |   \\|  | |  .--.  |  |  |  |");
+        System.out.println("|   _  <  |   __|  |  |\\/|  |   \\      /   |  | |  . `  | |  |  |  |  |  |  |");
+        System.out.println("|  |_)  | |  |____ |  |  |  |    \\    /    |  | |  |\\   | |  '--'  |  `--'  |");
+        System.out.println("|______/  |_______||__|  |__|     \\__/     |__| |__| \\__| |_______/ \\______/ ");
+        
+        System.out.println();
+        System.out.println("                             ___       ______                                                         ");
+        System.out.println("                            /   \\     /  __  \\                                                        ");
+        System.out.println("                           /  ^  \\   |  |  |  |                                                       ");
+        System.out.println("                          /  /_\\  \\  |  |  |  |                                                       ");
+        System.out.println("                         /  _____  \\ |  `--'  |                                                       ");
+        System.out.println("                        /__/     \\__\\ \\______/                                                        ");
+        
+        System.out.println("                                                                             ");
+        System.out.println("____    __    ____  _______   _______      ______   .__   __.  _______       ");
+        System.out.println("\\   \\  /  \\  /   / |   ____| /  _____|    /  __  \\  |  \\ |  | |   ____|      ");
+        System.out.println(" \\   \\/    \\/   /  |  |__   |  |  __     |  |  |  | |   \\|  | |  |__         ");
+        System.out.println("  \\            /   |   __|  |  | |_ |    |  |  |  | |  . `  | |   __|        ");
+        System.out.println("   \\    /\\    /    |  |____ |  |__| |    |  `--'  | |  |\\   | |  |____       ");
+        System.out.println("    \\__/  \\__/     |_______| \\______|     \\______/  |__| \\__| |_______|      ");
+        
     }
 }

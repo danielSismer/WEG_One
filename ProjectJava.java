0092138878b01402d3545974cfbbcb3b02b5b622
jose_a_torres <jose_a_torres@estudante.sesisenai.org.br>
2025-04-15 21:21:05 -0300

    feat: Implementa o menu

--- ProjectJava.java
+++ ProjectJava.java
@@ -1,35 +1,37 @@
 import java.util.Scanner;
 
 public class ProjectJava {
-    
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner (System.in);
 
-        System.out.println("Hello World");
-        
-        System.out.println(".______    _______ .___  ___. ____    ____  __  .__   __.  _______   ______  ");
-        System.out.println("|   _  \\  |   ____||   \\/   | \\   \\  /   / |  | |  \\ |  | |       \\ /  __  \\ ");
-        System.out.println("|  |_)  | |  |__   |  \\  /  |  \\   \\/   /  |  | |   \\|  | |  .--.  |  |  |  |");
-        System.out.println("|   _  <  |   __|  |  |\\/|  |   \\      /   |  | |  . `  | |  |  |  |  |  |  |");
-        System.out.println("|  |_)  | |  |____ |  |  |  |    \\    /    |  | |  |\\   | |  '--'  |  `--'  |");
-        System.out.println("|______/  |_______||__|  |__|     \\__/     |__| |__| \\__| |_______/ \\______/ ");
-        
-        System.out.println();
-        System.out.println("                             ___       ______                                                         ");
-        System.out.println("                            /   \\     /  __  \\                                                        ");
-        System.out.println("                           /  ^  \\   |  |  |  |                                                       ");
-        System.out.println("                          /  /_\\  \\  |  |  |  |                                                       ");
-        System.out.println("                         /  _____  \\ |  `--'  |                                                       ");
-        System.out.println("                        /__/     \\__\\ \\______/                                                        ");
-        
-        System.out.println("                                                                             ");
         System.out.println("____    __    ____  _______   _______      ______   .__   __.  _______       ");
         System.out.println("\\   \\  /  \\  /   / |   ____| /  _____|    /  __  \\  |  \\ |  | |   ____|      ");
         System.out.println(" \\   \\/    \\/   /  |  |__   |  |  __     |  |  |  | |   \\|  | |  |__         ");
         System.out.println("  \\            /   |   __|  |  | |_ |    |  |  |  | |  . `  | |   __|        ");
         System.out.println("   \\    /\\    /    |  |____ |  |__| |    |  `--'  | |  |\\   | |  |____       ");
         System.out.println("    \\__/  \\__/     |_______| \\______|     \\______/  |__| \\__| |_______|      ");
-        
+
+
+        System.out.println("\n==================================================================================\n");
+        int opcao;
+        String[] menuOpcoes = {" 1- Cadastrar Orientação: ", " 2- Pesquisar Orientação: ", " 3- Editar Orientação: ", " 4- Excluir Orientação: ", " 5- Sair: "};
+
+
+        do {
+            for (int i = 0; i < menuOpcoes.length; i++) {
+                System.out.println(menuOpcoes[i]);
+            }
+
+            System.out.print(" Digite aqui: ");
+            opcao = input.nextInt();
+
+
+
+        }while(opcao != 5);
+
+
+
     }
 }

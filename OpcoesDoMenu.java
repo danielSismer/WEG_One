37226f007a100b1252b6b1853f412dad9666ab38
JOSÉ AZARÍAS PÉREZ TORRES <jose_a_torres@estudante.sesisenai.org.br>
2025-04-28 18:30:42 -0300

    feat/ Inserir botão de voltar

--- OpcoesDoMenu.java
+++ OpcoesDoMenu.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class OpcoesDoMenu {
 
@@ -11,24 +13,99 @@ public class OpcoesDoMenu {
         JFrame telaDeCadastro = new JFrame("Cadastro");
         telaDeCadastro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         telaDeCadastro.setSize(900, 600);
+        telaDeCadastro.setExtendedState(JFrame.MAXIMIZED_BOTH);
         telaDeCadastro.setLayout(new BorderLayout());
         telaDeCadastro.setVisible(true);
 
+        JPanel menuLateral = new JPanel();
+        menuLateral.setBackground(new Color(0, 87, 156));
+        menuLateral.setLayout(new BoxLayout(menuLateral, BoxLayout.Y_AXIS));
+        menuLateral.setPreferredSize(new Dimension(200, telaDeCadastro.getHeight()));
+        menuLateral.setVisible(false);
+
+        JPanel headerPanel = new JPanel(new BorderLayout());
+        headerPanel.setBackground(new Color(0, 87, 156));
+        headerPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        telaDeCadastro.add(headerPanel, BorderLayout.NORTH);
+
+        JButton botaoIdioma = new JButton("\uD83C\uDF10");
+        botaoIdioma.setFont(new Font("SansSerif", Font.PLAIN, 30));
+        botaoIdioma.setFocusPainted(false);
+        botaoIdioma.setBackground(new Color(0, 87, 156));
+        botaoIdioma.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        botaoIdioma.setForeground(Color.WHITE);
+        headerPanel.add(botaoIdioma, BorderLayout.EAST);
+
+        JButton botaoVoltar = new JButton("◀\uFE0F");
+        botaoVoltar.setFont(new Font("SansSerif", Font.PLAIN, 30));
+        botaoVoltar.setBackground(new Color(0, 87, 156));
+        botaoVoltar.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        botaoVoltar.setFocusPainted(false);
+        botaoVoltar.setForeground(new Color(255, 255, 255));
+        headerPanel.add(botaoVoltar, BorderLayout.WEST);
+        botaoVoltar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                telaDeCadastro.setVisible(false);
+
+            }
+        });
 
     }
+
+
+
+
 
     public void ClickPesquisar() {
 
         JFrame telaDePesquisar = new JFrame("Pesquisar Orientação");
         telaDePesquisar.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         telaDePesquisar.setSize(900, 600);
+        telaDePesquisar.setExtendedState(JFrame.MAXIMIZED_BOTH);
         telaDePesquisar.setLayout(new BorderLayout());
         telaDePesquisar.setVisible(true);
 
+        JPanel menuLateral = new JPanel();
+        menuLateral.setBackground(new Color(0, 87, 156));
+        menuLateral.setLayout(new BoxLayout(menuLateral, BoxLayout.Y_AXIS));
+        menuLateral.setPreferredSize(new Dimension(200, telaDePesquisar.getHeight()));
+        menuLateral.setVisible(false);
 
-    }
+        JPanel headerPanel = new JPanel(new BorderLayout());
+        headerPanel.setBackground(new Color(0, 87, 156));
+        headerPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        telaDePesquisar.add(headerPanel, BorderLayout.NORTH);
 
-    ;
+        JButton botaoIdioma = new JButton("\uD83C\uDF10");
+        botaoIdioma.setFont(new Font("SansSerif", Font.PLAIN, 30));
+        botaoIdioma.setFocusPainted(false);
+        botaoIdioma.setBackground(new Color(0, 87, 156));
+        botaoIdioma.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        botaoIdioma.setForeground(Color.WHITE);
+        headerPanel.add(botaoIdioma, BorderLayout.EAST);
+
+        JButton botaoVoltar = new JButton("◀\uFE0F");
+        botaoVoltar.setFont(new Font("SansSerif", Font.PLAIN, 30));
+        botaoVoltar.setBackground(new Color(0, 87, 156));
+        botaoVoltar.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        botaoVoltar.setFocusPainted(false);
+        botaoVoltar.setForeground(new Color(255, 255, 255));
+        headerPanel.add(botaoVoltar, BorderLayout.WEST);
+        botaoVoltar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                telaDePesquisar.setVisible(false);
+
+            }
+        });
+    };
+
+
+
+
 
 
     public void ClickEditar() {
@@ -36,19 +113,98 @@ public class OpcoesDoMenu {
         JFrame telaDeEditar = new JFrame("Editar Orientação");
         telaDeEditar.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         telaDeEditar.setSize(900, 600);
+        telaDeEditar.setExtendedState(JFrame.MAXIMIZED_BOTH);
         telaDeEditar.setLayout(new BorderLayout());
         telaDeEditar.setVisible(true);
 
+        JPanel menuLateral = new JPanel();
+        menuLateral.setBackground(new Color(0, 87, 156));
+        menuLateral.setLayout(new BoxLayout(menuLateral, BoxLayout.Y_AXIS));
+        menuLateral.setPreferredSize(new Dimension(200, telaDeEditar.getHeight()));
+        menuLateral.setVisible(false);
+
+        JPanel headerPanel = new JPanel(new BorderLayout());
+        headerPanel.setBackground(new Color(0, 87, 156));
+        headerPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        telaDeEditar.add(headerPanel, BorderLayout.NORTH);
+
+        JButton botaoIdioma = new JButton("\uD83C\uDF10");
+        botaoIdioma.setFont(new Font("SansSerif", Font.PLAIN, 30));
+        botaoIdioma.setFocusPainted(false);
+        botaoIdioma.setBackground(new Color(0, 87, 156));
+        botaoIdioma.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        botaoIdioma.setForeground(Color.WHITE);
+        headerPanel.add(botaoIdioma, BorderLayout.EAST);
+
+        JButton botaoVoltar = new JButton("◀\uFE0F");
+        botaoVoltar.setFont(new Font("SansSerif", Font.PLAIN, 30));
+        botaoVoltar.setBackground(new Color(0, 87, 156));
+        botaoVoltar.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        botaoVoltar.setFocusPainted(false);
+        botaoVoltar.setForeground(new Color(255, 255, 255));
+        headerPanel.add(botaoVoltar, BorderLayout.WEST);
+        botaoVoltar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                telaDeEditar.setVisible(false);
+
+            }
+        });
     }
+
+
+
+
+
 
     public void ClickExcluir() {
 
         JFrame telaDeExcluir = new JFrame("Excluir Orientação");
         telaDeExcluir.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         telaDeExcluir.setSize(900, 600);
+        telaDeExcluir.setExtendedState(JFrame.MAXIMIZED_BOTH);
         telaDeExcluir.setLayout(new BorderLayout());
         telaDeExcluir.setVisible(true);
+
+        JPanel menuLateral = new JPanel();
+        menuLateral.setBackground(new Color(0, 87, 156));
+        menuLateral.setLayout(new BoxLayout(menuLateral, BoxLayout.Y_AXIS));
+        menuLateral.setPreferredSize(new Dimension(200, telaDeExcluir.getHeight()));
+        menuLateral.setVisible(false);
+
+        JPanel headerPanel = new JPanel(new BorderLayout());
+        headerPanel.setBackground(new Color(0, 87, 156));
+        headerPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        telaDeExcluir.add(headerPanel, BorderLayout.NORTH);
+
+        JButton botaoIdioma = new JButton("\uD83C\uDF10");
+        botaoIdioma.setFont(new Font("SansSerif", Font.PLAIN, 30));
+        botaoIdioma.setFocusPainted(false);
+        botaoIdioma.setBackground(new Color(0, 87, 156));
+        botaoIdioma.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        botaoIdioma.setForeground(Color.WHITE);
+        headerPanel.add(botaoIdioma, BorderLayout.EAST);
+
+        JButton botaoVoltar = new JButton("◀\uFE0F");
+        botaoVoltar.setFont(new Font("SansSerif", Font.PLAIN, 30));
+        botaoVoltar.setBackground(new Color(0, 87, 156));
+        botaoVoltar.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        botaoVoltar.setFocusPainted(false);
+        botaoVoltar.setForeground(new Color(255, 255, 255));
+        headerPanel.add(botaoVoltar, BorderLayout.WEST);
+        botaoVoltar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                telaDeExcluir.setVisible(false);
+
+            }
+        });
     }
+
+
+
 
     public void ClickSair() {
         System.exit(0);

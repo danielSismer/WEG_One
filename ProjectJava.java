24847b4710372d62c7c0972d2d0c0842c88cbb60
DANIEL VINICIUS RIOS SISMER <daniel_sismer@estudante.sesisenai.org.br>
2025-04-28 19:20:14 -0300

    Fix: PJ-37 Renomeando objetos de classes

--- ProjectJava.java
+++ ProjectJava.java
@@ -5,77 +5,47 @@ import java.util.Scanner;
 
 public class ProjectJava {
 
-
     public static void main(String[] args) {
-
 
         Scanner input = new Scanner(System.in);
 
+        String[] itensManualDeOperacao = new String[20],  itensManualCondutaSetorial = new String[20], itensProcedimentodeSeguranca = new String[20], itensTesteDiagnostico = new String[20], itensManutencaoReparo = new String[20];
 
-        // objetos para a classe manual de operação
         ManualDeOperacao manualMotor = new ManualDeOperacao();
         ManualDeOperacao manualGerador = new ManualDeOperacao();
-
-
-        String[] itensManualDeOperacao = new String[20];
-
 
         itensManualDeOperacao[0] = manualMotor.nomeProduto;
         itensManualDeOperacao[1] = manualGerador.nomeProduto;
 
 
-        // objetos para a classe procedimentos de segurança
         ProcedimentodeSeguranca procedimentoEletrico = new ProcedimentodeSeguranca();
         ProcedimentodeSeguranca procedimentoChoque = new ProcedimentodeSeguranca();
-
-
-        String[] itensProcedimentodeSeguranca = new String[20];
-
 
         itensProcedimentodeSeguranca[0] = procedimentoChoque.nomeProduto;
         itensProcedimentodeSeguranca[1] = procedimentoEletrico.nomeProduto;
 
 
-        ManutencaoReparo manutencaoReparo01 = new ManutencaoReparo();
-        ManutencaoReparo manutencaoReparo02 = new ManutencaoReparo();
+        ManutencaoReparo manutencaoReparoMotorEletrico = new ManutencaoReparo();
+        ManutencaoReparo manutencaoReparoTransformadores = new ManutencaoReparo();
+
+        itensManutencaoReparo [0] = manutencaoReparoMotorEletrico.nomeProduto;
+        itensManutencaoReparo [1] = manutencaoReparoTransformadores.nomeProduto;
 
 
+        TesteDiagnostico testeDiagnosticoMotores = new TesteDiagnostico();
+        TesteDiagnostico testeDiagnosticoFalhas = new TesteDiagnostico();
+
+        itensTesteDiagnostico [0] = testeDiagnosticoMotores.nomeProduto;
+        itensTesteDiagnostico [1] = testeDiagnosticoFalhas.nomeProduto;
 
 
+        ManualCondutaSetorial manualCondutaSetorialMontagem = new ManualCondutaSetorial();
+        ManualCondutaSetorial manualCondutaSetorialManutencao = new ManualCondutaSetorial();
+
+        itensManualCondutaSetorial [0] = manualCondutaSetorialMontagem.nomeSetor;
+        itensManualCondutaSetorial [1] = manualCondutaSetorialManutencao.nomeSetor;
 
 
-        String [] itensManutencaoReparo = new String[20];
-
-
-        itensManutencaoReparo [0] = manutencaoReparo01.nomeProduto;
-        itensManutencaoReparo [1] = manutencaoReparo02.nomeProduto;
-
-
-
-
-        TesteDiagnostico testeDiagnostico1 = new TesteDiagnostico();
-        TesteDiagnostico testeDiagnostico2 = new TesteDiagnostico();
-
-
-        String [] itensTesteDiagnostico = new String[20];
-
-
-        itensTesteDiagnostico [0] = testeDiagnostico1.nomeProduto;
-        itensTesteDiagnostico [1] = testeDiagnostico2.nomeProduto;
-
-
-        ManualCondutaSetorial manualCondutaSetorial01 = new ManualCondutaSetorial();
-        ManualCondutaSetorial manualCondutaSetorial2 = new ManualCondutaSetorial();
-
-
-        String [] itensManualCondutaSetorial = new String[20];
-
-
-        itensManualCondutaSetorial [0] = manualCondutaSetorial01.nomeSetor;
-        itensManualCondutaSetorial [1] = manualCondutaSetorial2.nomeSetor;
-
-
-        // setar valores para manualMotor
         manualMotor.setNomeProduto("Motores Elétricos WEG");
         manualMotor.setComoUsar("Antes de iniciar a operação do motor, verifique a integridade dos cabos de alimentação " +
                 "e a conexão correta à rede elétrica. Aumente gradualmente a carga no motor para evitar picos de corrente no início da " +
@@ -84,7 +54,6 @@ public class ProjectJava {
                 "do fabricante para evitar sobrecarga no momento da partida.\n");
 
 
-        //setar valores para manualGerador
         manualGerador.setNomeProduto("Operação de Geradores WEG");
         manualGerador.setComoUsar("Descrição: Certifique-se de que o gerador está corretamente instalado e com os níveis de óleo e combustível adequados. Durante a " +
                 "operação, monitore constantemente os parâmetros de tensão e corrente. Evite sobrecarga, pois pode danificar o gerador e" +
@@ -93,57 +62,51 @@ public class ProjectJava {
                 " a performance e o status dos componentes, como nível de combustível e temperatura.");
 
 
-        // setar valores para Procedimentoeletrico
+
         procedimentoEletrico.setNomeProduto("Procedimento de Segurança para Instalação de Equipamentos Elétricos");
         procedimentoEletrico.setComoUsar("Descrição: Antes de iniciar a instalação de qualquer equipamento elétrico, como motores e transformadores WEG, desconecte toda a alimentação elétrica e realize o aterramento adequado. Utilize ferramentas isoladas e verifique o ambiente quanto à presença de materiais inflamáveis ou risco de curto-circuito.");
         procedimentoEletrico.setExemploPratico("Exemplo Prático: Durante a instalação de um transformador WEG, garanta que o painel de controle esteja devidamente identificado e sinalizado para evitar conexões erradas que possam causar falhas no sistema.");
 
 
-        // setar valores para procedimentosChoque
+
         procedimentoChoque.setNomeProduto("Operações com Risco de Choque Elétrico");
         procedimentoEletrico.setComoUsar("Descrição: Sempre use equipamentos de proteção individual (EPIs) adequados, como luvas isolantes e botas de segurança, antes de realizar qualquer manutenção ou inspeção em equipamentos energizados. Durante a operação de equipamentos de alta tensão, siga os procedimentos de bloqueio e etiquetagem (LOTO) para garantir que o sistema esteja desligado.");
         procedimentoChoque.setExemploPratico("Exemplo Prático: No caso de manutenção de transformadores WEG de alta tensão, desconecte a alimentação elétrica, utilize chaves de bloqueio e verifique com um multímetro a ausência de tensão nos terminais.");
 
 
-        // setar valores para manutenção motores eletrico
-        manutencaoReparo01.setNomeProduto("Manutenção de Motores Elétricos");
-        manutencaoReparo01.setDescricaoManutencao("Descrição: Realize inspeções regulares nos rolamentos, verificando ruídos anormais ou aquecimento excessivo. Lubrifique os rolamentos conforme as instruções do fabricante e substitua-os se necessário. Verifique a conexão dos terminais elétricos e a presença de sinais de desgaste ou corrosão.");
-        manutencaoReparo01.setExemploPratico("Exemplo Prático: Para motores WEG, recomenda-se a realização de manutenção preventiva a cada 1.000 horas de operação, incluindo a verificação do alinhamento do motor e o teste de resistência do isolamento.");
+        manutencaoReparoMotorEletrico.setNomeProduto("Manutenção de Motores Elétricos");
+        manutencaoReparoMotorEletrico.setDescricaoManutencao("Descrição: Realize inspeções regulares nos rolamentos, verificando ruídos anormais ou aquecimento excessivo. Lubrifique os rolamentos conforme as instruções do fabricante e substitua-os se necessário. Verifique a conexão dos terminais elétricos e a presença de sinais de desgaste ou corrosão.");
+        manutencaoReparoMotorEletrico.setExemploPratico("Exemplo Prático: Para motores WEG, recomenda-se a realização de manutenção preventiva a cada 1.000 horas de operação, incluindo a verificação do alinhamento do motor e o teste de resistência do isolamento.");
 
 
-        // setar valores para Reparo de Transformadores
-        manutencaoReparo02.setNomeProduto("Reparo de Transformadores");
-        manutencaoReparo02.setDescricaoManutencao("Descrição: Quando realizar reparos em transformadores WEG, sempre faça uma análise cuidadosa da situação de cada componente, como bobinas e buchas. Caso haja falha nas buchas ou nos isoladores, substitua-os imediatamente. Em caso de curto-circuito no interior do transformador, realize testes de resistência de isolamento e verifique o nível de óleo.");
-        manutencaoReparo02.setExemploPratico("Exemplo Prático: Se um transformador WEG apresentar aquecimento excessivo, além de verificar o nível de óleo, examine o radiador e limpe as ventoinhas para garantir uma boa dissipação de calor.");
+        manutencaoReparoTransformadores.setNomeProduto("Reparo de Transformadores");
+        manutencaoReparoTransformadores.setDescricaoManutencao("Descrição: Quando realizar reparos em transformadores WEG, sempre faça uma análise cuidadosa da situação de cada componente, como bobinas e buchas. Caso haja falha nas buchas ou nos isoladores, substitua-os imediatamente. Em caso de curto-circuito no interior do transformador, realize testes de resistência de isolamento e verifique o nível de óleo.");
+        manutencaoReparoTransformadores.setExemploPratico("Exemplo Prático: Se um transformador WEG apresentar aquecimento excessivo, além de verificar o nível de óleo, examine o radiador e limpe as ventoinhas para garantir uma boa dissipação de calor.");
 
 
-        // setar valores para funcionalidade de motor
-        testeDiagnostico1.setNomeProduto("Testes de Funcionamento de Motores");
-        testeDiagnostico1.setDescricaoDiagnostico("Descrição: Para garantir que o motor WEG está operando corretamente, execute testes de partida em vazio, monitorando a corrente elétrica e a rotação. Após a operação, verifique a temperatura do motor e as vibrações para identificar possíveis falhas mecânicas.");
-        testeDiagnostico1.setexemploPratico("Exemplo Prático: Durante os testes de carga, monitore a eficiência do motor e se a corrente está dentro dos parâmetros especificados pelo fabricante para evitar danos ao equipamento");
+
+        testeDiagnosticoMotores.setNomeProduto("Testes de Funcionamento de Motores");
+        testeDiagnosticoMotores.setDescricaoDiagnostico("Descrição: Para garantir que o motor WEG está operando corretamente, execute testes de partida em vazio, monitorando a corrente elétrica e a rotação. Após a operação, verifique a temperatura do motor e as vibrações para identificar possíveis falhas mecânicas.");
+        testeDiagnosticoMotores.setexemploPratico("Exemplo Prático: Durante os testes de carga, monitore a eficiência do motor e se a corrente está dentro dos parâmetros especificados pelo fabricante para evitar danos ao equipamento");
 
 
-        // setar valores para Diagnóstico de Falhas
-
-
-        testeDiagnostico2.setNomeProduto("Diagnóstico de Falhas em Sistemas de Automação");
-        testeDiagnostico2.setDescricaoDiagnostico("Descrição: No diagnóstico de falhas em sistemas de automação da WEG, como controladores e inversores de frequência, utilize ferramentas de diagnóstico de comunicação como modbus ou ethernet para verificar possíveis falhas de comunicação. Revise os logs de erros e teste os sensores e atuadores para garantir que todos os sistemas estão funcionando corretamente.");
-        testeDiagnostico2.setexemploPratico("Exemplo Prático: Para sistemas de automação WEG com inversores de frequência, execute um teste de carga variável para identificar falhas em processos de controle de velocidade e torque.");
-
+        testeDiagnosticoFalhas.setNomeProduto("Diagnóstico de Falhas em Sistemas de Automação");
+        testeDiagnosticoFalhas.setDescricaoDiagnostico("Descrição: No diagnóstico de falhas em sistemas de automação da WEG, como controladores e inversores de frequência, utilize ferramentas de diagnóstico de comunicação como modbus ou ethernet para verificar possíveis falhas de comunicação. Revise os logs de erros e teste os sensores e atuadores para garantir que todos os sistemas estão funcionando corretamente.");
+        testeDiagnosticoFalhas.setexemploPratico("Exemplo Prático: Para sistemas de automação WEG com inversores de frequência, execute um teste de carga variável para identificar falhas em processos de controle de velocidade e torque.");
 
 
 
         // setar valores para manual de conduta
-        manualCondutaSetorial01.setNomeSetor("Conduta no Setor de Montagem");
-        manualCondutaSetorial01.setCondutaEsperada("No setor de montagem, os colaboradores devem seguir rigorosamente os procedimentos de segurança e as normas de qualidade da WEG. Devem garantir que as ferramentas e equipamentos estejam sempre em boas condições, realizando inspeções periódicas e manutenções preventivas.");
-        manualCondutaSetorial01.setObservacoes("Ao montar motores elétricos WEG, é fundamental verificar se as peças estão corretamente alinhadas e se todas as conexões elétricas estão firmemente apertadas, prevenindo falhas e retrabalho.");
+        manualCondutaSetorialMontagem.setNomeSetor("Conduta no Setor de Montagem");
+        manualCondutaSetorialMontagem.setCondutaEsperada("No setor de montagem, os colaboradores devem seguir rigorosamente os procedimentos de segurança e as normas de qualidade da WEG. Devem garantir que as ferramentas e equipamentos estejam sempre em boas condições, realizando inspeções periódicas e manutenções preventivas.");
+        manualCondutaSetorialMontagem.setObservacoes("Ao montar motores elétricos WEG, é fundamental verificar se as peças estão corretamente alinhadas e se todas as conexões elétricas estão firmemente apertadas, prevenindo falhas e retrabalho.");
 
 
         // setar valores para Procedimentos Operacionais
-        manualCondutaSetorial2.setNomeSetor("Procedimentos Operacionais no Setor de Manutenção");
-        manualCondutaSetorial2.setCondutaEsperada("Conduta Esperada:\n" +
+        manualCondutaSetorialManutencao.setNomeSetor("Procedimentos Operacionais no Setor de Manutenção");
+        manualCondutaSetorialManutencao.setCondutaEsperada("Conduta Esperada:\n" +
                 "No setor de manutenção, os técnicos devem seguir processos específicos de diagnóstico e reparo, sempre utilizando os EPIs adequados e ferramentas apropriadas. É obrigatório registrar todas as manutenções e trocas de componentes no sistema de gestão da empresa de forma precisa e atualizada.");
-        manualCondutaSetorial2.setObservacoes(" Durante a manutenção de um inversor de frequência WEG, o técnico deve inspecionar cuidadosamente os componentes eletrônicos internos (como resistores e capacitores) e realizar testes de isolamento, assegurando a integridade e segurança do equipamento. ");
+        manualCondutaSetorialManutencao.setObservacoes(" Durante a manutenção de um inversor de frequência WEG, o técnico deve inspecionar cuidadosamente os componentes eletrônicos internos (como resistores e capacitores) e realizar testes de isolamento, assegurando a integridade e segurança do equipamento. ");
 
 
 

79c50c26195fb12927f105d972dae2aa8e26a0f8
DANIEL VINICIUS RIOS SISMER <daniel_sismer@estudante.sesisenai.org.br>
2025-05-12 21:42:11 -0300

    Realizamos alguns ajustes de edição e erros de compilação. ( Daniel, Kael e gabriel)

--- src/main/java/net/weg/services/ManutencaoReparo.java
+++ src/main/java/net/weg/services/ManutencaoReparo.java
@@ -1,4 +1,5 @@
 package net.weg.services;
+import net.weg.services.ProjectJava;
 
 import java.util.Scanner;
 
@@ -54,6 +55,87 @@ public class ManutencaoReparo {
 
         return "Produto cadastrado: " + this.nomeProduto + " - " + this.descricaoManutencao + "-" + this.exemploPratico;
 
+
+    }
+
+
+    public void editar(int escolhaIdiona) {
+
+
+            Scanner input = new Scanner(System.in);
+
+        if (escolhaIdiona == 1) {
+
+            System.out.print("Welches Element möchten Sie ändern?\n1 - Name der Anleitung\n2 - Verwendung\n3 - Praktisches Beispiel\n");
+            int escolhaEdit = input.nextInt();
+
+            if (escolhaEdit == 1) {
+
+                System.out.print("\nGeben Sie den Namen des Produkts ein: ");
+                this.nomeProduto = input.nextLine();
+
+            } else if (escolhaEdit == 2) {
+
+                System.out.print("Geben Sie die Beschreibung des Produkts ein: ");
+                this.descricaoManutencao = input.nextLine();
+
+            } else if (escolhaEdit == 3) {
+
+                System.out.print("Geben Sie ein praktisches Beispiel ein: ");
+                this.exemploPratico = input.nextLine();
+
+            }
+
+            System.out.println("\nAnleitung erfolgreich bearbeitet!");
+
+        } else if (escolhaIdiona == 2) {
+
+            System.out.print("Qual item deseja modificar?\n1 - Nome da Orientação\n2 - Como Usar\n3 - Exemplo Prático\n");
+            int escolhaEdit = input.nextInt();
+
+            if (escolhaEdit == 1) {
+
+                System.out.print("\nDigite o nome do produto: ");
+                this.nomeProduto = input.nextLine();
+
+            } else if (escolhaEdit == 2) {
+
+                System.out.print("Digite a descrição do produto: ");
+                this.descricaoManutencao = input.nextLine();
+
+            } else if (escolhaEdit == 3) {
+
+                System.out.print("Digite um exemplo prático: ");
+                this.exemploPratico = input.nextLine();
+
+            }
+
+            System.out.println("\nOrientação editada com sucesso!");
+
+        } else if (escolhaIdiona == 3) {
+
+            System.out.print("Which item would you like to modify?\n1 - Instruction Name\n2 - How to Use\n3 - Practical Example\n");
+            int escolhaEdit = input.nextInt();
+
+            if (escolhaEdit == 1) {
+
+                System.out.print("\nEnter the product name: ");
+                this.nomeProduto = input.nextLine();
+
+            } else if (escolhaEdit == 2) {
+
+                System.out.print("Enter the product description: ");
+                this.descricaoManutencao = input.nextLine();
+
+            } else if (escolhaEdit == 3) {
+
+                System.out.print("Enter a practical example: ");
+                this.exemploPratico = input.nextLine();
+
+            }
+
+            System.out.println("\nInstruction successfully edited!");
+        }
 
     }
 }
--- src/main/java/net/weg/services/ProcedimentodeSeguranca.java
+++ src/main/java/net/weg/services/ProcedimentodeSeguranca.java
@@ -1,8 +1,11 @@
 package net.weg.services;
+import net.weg.services.ProjectJava;
 
 import java.util.Scanner;
 
 public class ProcedimentodeSeguranca {
+
+    Scanner input = new Scanner(System.in);
 
     String nomeProduto;
     String comoUsar;
@@ -55,6 +58,84 @@ public class ProcedimentodeSeguranca {
 
 
     }
+    public void editar(int escolhaIdiona) {
+        Scanner scanner = new Scanner(System.in);
+
+        if (escolhaIdiona == 1) {
+
+            System.out.print("Welches Element möchten Sie ändern?\n1 - Name der Anleitung\n2 - Verwendung\n3 - Praktisches Beispiel\n");
+            int escolhaEdit = input.nextInt();
+
+            if (escolhaEdit == 1) {
+
+                System.out.print("\nGeben Sie den Namen des Produkts ein: ");
+                this.nomeProduto = scanner.nextLine();
+
+            } else if (escolhaEdit == 2) {
+
+                System.out.print("Geben Sie die Beschreibung des Produkts ein: ");
+                this.comoUsar = scanner.nextLine();
+
+            } else if (escolhaEdit == 3) {
+
+                System.out.print("Geben Sie ein praktisches Beispiel ein: ");
+                this.exemploPratico = scanner.nextLine();
+
+            }
+
+            System.out.println("\nAnleitung erfolgreich bearbeitet!");
+
+        } else if (escolhaIdiona == 2) {
+
+            System.out.print("Qual item deseja modificar?\n1 - Nome da Orientação\n2 - Como Usar\n3 - Exemplo Prático\n");
+            int escolhaEdit = input.nextInt();
+
+            if (escolhaEdit == 1) {
+
+                System.out.print("\nDigite o nome do produto: ");
+                this.nomeProduto = scanner.nextLine();
+
+            } else if (escolhaEdit == 2) {
+
+                System.out.print("Digite a descrição do produto: ");
+                this.comoUsar = scanner.nextLine();
+
+            } else if (escolhaEdit == 3) {
+
+                System.out.print("Digite um exemplo prático: ");
+                this.exemploPratico = scanner.nextLine();
+
+            }
+
+            System.out.println("\nOrientação editada com sucesso!");
+
+        } else if (escolhaIdiona == 3) {
+
+            System.out.print("Which item would you like to modify?\n1 - Instruction Name\n2 - How to Use\n3 - Practical Example\n");
+            int escolhaEdit = input.nextInt();
+
+            if (escolhaEdit == 1) {
+
+                System.out.print("\nEnter the product name: ");
+                this.nomeProduto = scanner.nextLine();
+
+            } else if (escolhaEdit == 2) {
+
+                System.out.print("Enter the product description: ");
+                this.comoUsar = scanner.nextLine();
+
+            } else if (escolhaEdit == 3) {
+
+                System.out.print("Enter a practical example: ");
+                this.exemploPratico = scanner.nextLine();
+
+            }
+
+            System.out.println("\nInstruction successfully edited!");
+        }
+
+    }
+
 
 
 }

11ce1f367f07ff524b2bfc6b380601b7b48c6ed2
Gabriel Ehrat <gabriel_e_fagundes@estudante.sesisenai.org.br>
2025-05-27 14:01:50 -0300

    PJ-85 e PJ-83: Adicionando a opção de navegar entre idiomas e adicionando a opção de navegar pelo visual ou terminal. (#50)
    * fix: Arrumando a Apresentacao.java
    
    * feat: Adicionando o GIF no README e adicionando a alteração do idioma
    
    * fix: Atualizando o GIF do README
    
    * fix: Alterando o caminho para a interface visual
    
    * feat: Adicionando uma LOGO para o terminal

--- src/main/java/net/weg/Apresentacao.java
+++ src/main/java/net/weg/Apresentacao.java
@@ -1,4 +1,28 @@
 package net.weg;
+import net.weg.services.ProjectJava;
+import net.weg.apresentation.TelaDeslogada;
+
+import javax.swing.JOptionPane;
 
 public class Apresentacao {
+
+    public static void main(String[] args) {
+
+        int escolha = JOptionPane.showConfirmDialog(null, "Deseja iniciar o projeto visual?", "Visual ou Terminal", JOptionPane.YES_NO_OPTION);
+
+        if (escolha == JOptionPane.YES_OPTION) {
+
+            TelaDeslogada.main(null);
+
+        }else if(escolha == JOptionPane.NO_OPTION){
+
+            ProjectJava.main(null);
+
+        }
+
+    }
+
 }
+
+
+
